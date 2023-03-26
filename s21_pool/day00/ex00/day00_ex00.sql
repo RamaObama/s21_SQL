13836d73d006131ad26d250023f2099c9367f692
rama <i@rrmstu.ru>
2023-03-27 04:16:45 +0500

    :rotating_light: :: lint for sql query

--- s21_pool/day00/ex00/day00_ex00.sql
+++ s21_pool/day00/ex00/day00_ex00.sql
@@ -1,4 +1,6 @@
 -- Вариант № 1
 -- SELECT name, age FROM person WHERE address = 'Kazan';
 -- Вариант № 2
-SELECT per.name, per.age FROM person AS per WHERE per.address = 'Kazan';
+SELECT per.name, per.age
+FROM person AS per
+WHERE per.address = 'Kazan';
--- s21_pool/day00/ex01/day00_ex01.sql
+++ s21_pool/day00/ex01/day00_ex01.sql
@@ -1,4 +1,8 @@
 -- Вариант № 1
 -- SELECT name, age FROM person WHERE address = 'Kazan' AND gender = 'female' ORDER BY name;
 -- Вариант № 2
-SELECT per.name, per.age FROM person AS per WHERE per.address = 'Kazan' AыND per.gender = 'female' ORDER BY per.name;
+SELECT per.name, per.age
+FROM person AS per
+WHERE per.address = 'Kazan'
+  AND per.gender = 'female'
+ORDER BY per.name;
