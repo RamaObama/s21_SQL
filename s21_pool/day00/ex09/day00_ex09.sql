1bf07c051963452b449c3807ccdade1720d0a0cc
rama <i@rrmstu.ru>
2023-03-29 03:32:26 +0500

    :bug: :: fix order for pizzeria_name

--- s21_pool/day00/ex09/day00_ex09.sql
+++ s21_pool/day00/ex09/day00_ex09.sql
@@ -7,4 +7,4 @@ SELECT (SELECT person.name
 FROM (SELECT *
       FROM person_visits
       WHERE person_visits.visit_date BETWEEN '2022-01-07' AND '2022-01-09') AS pv
-ORDER BY person_name ASC, person_name DESC;
+ORDER BY person_name ASC, pizzeria_name DESC;
