f73e3e962b0c28ac8993d8eff3c6799b4e1cb227
rama <i@rrmstu.ru>
2023-03-28 23:33:32 +0500

    :bug: :: fix solution for task

--- s21_pool/day01/ex01/day01_ex01.sql
+++ s21_pool/day01/ex01/day01_ex01.sql
@@ -1,6 +1,15 @@
-SELECT mu.pizza_name AS object_name
-FROM menu AS mu
+(SELECT pers.name AS object_name
+ FROM person AS pers
+ ORDER BY object_name)
 UNION ALL
-SELECT pers.name AS object_name
-FROM person AS pers
-ORDER BY object_name;
+(SELECT mu.pizza_name AS object_name
+ FROM menu AS mu
+ ORDER BY object_name)
+
+-- MAC version???
+-- SELECT mu.id AS object_id, mu.pizza_name AS object_name
+-- FROM menu AS mu
+-- UNION
+-- SELECT pers.id AS object_id, pers.name AS object_id
+-- FROM person AS pers
+-- ORDER BY object_id, object_name;
