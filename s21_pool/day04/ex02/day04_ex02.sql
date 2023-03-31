8babb3c3286f00feef87dcdebc1bd1883c101bf7
rama <i@rrmstu.ru>
2023-04-01 04:31:08 +0500

    :adhesive_bandage: :: lint fix

--- s21_pool/day04/ex02/day04_ex02.sql
+++ s21_pool/day04/ex02/day04_ex02.sql
@@ -2,7 +2,7 @@
 -- Don’t forget about order for the generated_date column.
 
 CREATE VIEW v_generated_dates AS
-SELECT day::date AS g
+SELECT day::date AS generated_date
 FROM generate_series('2022-01-01'::date, '2022-01-31'::date, '1 day') day
 ORDER BY 1;
 
