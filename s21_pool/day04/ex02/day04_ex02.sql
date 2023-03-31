730a20b649dfc86608da7d06934c844ebde20d93
rama <i@rrmstu.ru>
2023-04-01 04:24:33 +0500

    :sparkles: ::  generated dates in one place

---
+++ s21_pool/day04/ex02/day04_ex02.sql
@@ -0,0 +1,9 @@
+-- Please create a Database View (with name v_generated_dates) which should be “store” generated dates from 1st to 31th of January 2022 in DATE type.
+-- Don’t forget about order for the generated_date column.
+
+CREATE VIEW v_generated_dates AS
+SELECT day::date AS g
+FROM generate_series('2022-01-01'::date, '2022-01-31'::date, '1 day') day
+ORDER BY 1;
+
+-- SELECT * FROM v_generated_dates;
