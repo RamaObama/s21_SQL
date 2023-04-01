d17ea0e572407c18f9c384bcbb1e22f1043103b4
rama <i@rrmstu.ru>
2023-04-01 20:26:38 +0500

    :sparkles: :: Classical TSP

--- s21_pool/team00/ex00/team00_ex00.sql
+++ s21_pool/team00/ex00/team00_ex00.sql
@@ -41,3 +41,32 @@ VALUES ('A', 'B', 10),
 SELECT *
 FROM nodes;
 
+CREATE VIEW way AS
+(
+WITH RECURSIVE tour AS (SELECT point1::bpchar AS tour,
+                               point1,
+                               point2,
+                               cost,
+                               cost           AS sum
+                        FROM nodes
+                        WHERE point1 = 'A'
+                        UNION ALL
+                        SELECT parent.tour || ',' || parent.point2 AS tour,
+                               child.point1,
+                               child.point2,
+                               parent.cost,
+                               parent.sum + child.cost             AS sum
+                        FROM nodes AS child
+                                 INNER JOIN tour AS parent ON child.point1 = parent.point2
+                        WHERE tour NOT LIKE '%' || parent.point2 || '%')
+SELECT *
+FROM tour
+    );
+
+SELECT sum                                     AS total_cost,
+       '{' || tour || ',' || way.point2 || '}' AS tour
+FROM way
+WHERE length(tour) = 7
+  AND point2 = 'A'
+  AND sum = (SELECT min(sum) FROM way WHERE length(tour) = 7 AND point2 = 'A')
+ORDER BY 1, 2;
