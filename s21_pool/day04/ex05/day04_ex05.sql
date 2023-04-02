b7660da0896bd05dbbe6878a9a0242a21d7f772d
rama <i@rrmstu.ru>
2023-04-02 18:16:48 +0500

    :adhesive_bandage: :: added order by for pizza name

--- s21_pool/day04/ex05/day04_ex05.sql
+++ s21_pool/day04/ex05/day04_ex05.sql
@@ -9,7 +9,7 @@ WITH orders AS (SELECT p.name, m.pizza_name, m.price
                          JOIN menu m on po.menu_id = m.id)
 SELECT *, round(price - price * 0.1) AS discount_price
 FROM orders
-ORDER BY name;
+ORDER BY name, pizza_name;
 
 -- SELECT * FROM v_price_with_discount;
 -- DROP VIEW v_price_with_discount;
