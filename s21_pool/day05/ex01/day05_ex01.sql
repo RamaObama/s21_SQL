bff27ab1a30266737e429e02a4c675ba6b90a27e
rama <i@rrmstu.ru>
2023-04-03 17:31:25 +0500

    :adhesive_bandage: :: fix ANALYSE => ANALYZE

--- s21_pool/day05/ex01/day05_ex01.sql
+++ s21_pool/day05/ex01/day05_ex01.sql
@@ -17,7 +17,7 @@ SELECT m.pizza_name,
 FROM menu m
          JOIN pizzeria p ON m.pizzeria_id = p.id;
 
-EXPLAIN ANALYSE
+EXPLAIN ANALYZE
 SELECT m.pizza_name,
        p.name AS pizzeria_name
 FROM menu m
