2c9138a0b425068ab12bd40f2e6e82e3a19c4740
rama <i@rrmstu.ru>
2023-04-03 21:22:35 +0500

    :sparkles: :: Partial uniqueness for data

---
+++ s21_pool/day05/ex05/day05_ex05.sql
@@ -0,0 +1,17 @@
+-- Please create a partial unique BTree index with the name idx_person_order_order_date
+-- on the person_order table for person_id and menu_id attributes with partial uniqueness
+-- for order_date column for date ‘2022-01-01’.
+--
+-- The EXPLAIN ANALYZE command should return  the next pattern
+
+CREATE UNIQUE INDEX idx_person_order_order_date
+    ON person_order (person_id, menu_id)
+    WHERE order_date = '2022-01-01';
+
+SET ENABLE_SEQSCAN = OFF;
+
+EXPLAIN ANALYZE
+SELECT person_id,
+       menu_id
+FROM person_order
+WHERE order_date = '2022-01-01';
