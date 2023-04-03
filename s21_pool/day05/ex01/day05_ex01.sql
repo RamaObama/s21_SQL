278f85fe98f8096b0fd73bc34dac24edd7cea041
rama <i@rrmstu.ru>
2023-04-03 17:22:24 +0500

    :sparkles: :: How to see that index works?

---
+++ s21_pool/day05/ex01/day05_ex01.sql
@@ -0,0 +1,24 @@
+-- Before further steps please write a SQL statement that returns pizzas’ and corresponding pizzeria names.
+-- Please take a look at the sample result below (no sort needed).
+--
+-- __________________________________
+-- |  pizza_name  |  pizzeria_name  |
+-- | cheese pizza |    Pizza Hut    |
+-- | ...          | ...             |
+-- ----------------------------------
+-- Let’s provide proof that your indexes are working for your SQL.
+-- The sample of proof is the output of the EXPLAIN ANALYZE command.
+-- Please take a look at the sample output command.
+
+SET ENABLE_SEQSCAN = OFF;
+
+SELECT m.pizza_name,
+       p.name
+FROM menu m
+         JOIN pizzeria p ON m.pizzeria_id = p.id;
+
+EXPLAIN ANALYSE
+SELECT m.pizza_name,
+       p.name
+FROM menu m
+         JOIN pizzeria p ON m.pizzeria_id = p.id;
