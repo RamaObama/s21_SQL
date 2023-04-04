f438c5586e1840b9738092768b2288b563ec46f1
rama <i@rrmstu.ru>
2023-04-04 23:13:13 +0500

    :sparkles: :: Let’s automate Primary Key generation

---
+++ s21_pool/day06/ex06/day06_ex06.sql
@@ -0,0 +1,11 @@
+-- Let’s create a Database Sequence with the name seq_person_discounts (starting from 1 value) and set a default value for id attribute of person_discounts table to take a value from seq_person_discounts each time automatically.
+-- Please be aware that your next sequence number is 1, in this case please set an actual value for database sequence based on formula “amount of rows in person_discounts table” + 1.
+-- Otherwise you will get errors about Primary Key violation constraint.
+
+CREATE SEQUENCE seq_person_discounts START 1;
+
+SELECT setval('seq_person_discounts', max(id))
+FROM person_discounts;
+
+ALTER TABLE person_discounts
+    ALTER COLUMN id SET default nextval('seq_person_discounts');
