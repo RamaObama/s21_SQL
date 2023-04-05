d1055fdb66f56dd80c66c95d2e36a3173be928ee
rama <i@rrmstu.ru>
2023-04-06 01:28:08 +0500

    :sparkles: :: Person's uniqueness

---
+++ s21_pool/day07/ex05/day07_ex05.sql
@@ -0,0 +1,7 @@
+-- Please write a simple SQL query that returns a list of unique person names who made orders in any pizzerias.
+-- The result should be sorted by person name.
+
+SELECT DISTINCT p.name
+FROM person_order po
+         JOIN person p on p.id = po.person_id
+ORDER BY 1;
