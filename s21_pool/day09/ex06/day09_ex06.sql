78fca875426bd2f41a36c4ea9404d4b21274322d
rama <i@rrmstu.ru>
2023-04-08 05:31:39 +0500

    :art: :: Two new solutions have been added. there are now [3]

--- s21_pool/day09/ex06/day09_ex06.sql
+++ s21_pool/day09/ex06/day09_ex06.sql
@@ -7,18 +7,19 @@
 -- (IN pdate parameter with default value is 8th of January 2022).
 -- To check yourself and call a function, you can make a statement like below.
 
+-- Вариант №1.
 CREATE FUNCTION fnc_person_visits_and_eats_on_date(
     IN pperson varchar DEFAULT 'Dmitriy',
     IN pprice numeric DEFAULT 500,
     IN pdate date DEFAULT '2022-01-08')
     RETURNS TABLE
             (
-                name varchar
+                pizzeria_name varchar
             )
 AS
 $$
 BEGIN
-    RETURN QUERY ((SELECT pz.name
+    RETURN QUERY ((SELECT pz.name AS pizzeria_name
                    FROM (SELECT *
                          FROM person_visits pv
                          WHERE pv.visit_date = pdate) visit
@@ -50,8 +51,77 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Вариант №2.
+CREATE OR REPLACE FUNCTION fnc_person_visits_and_eats_on_date_2(
+    pperson varchar DEFAULT 'Dmitriy',
+    pprice numeric DEFAULT 500,
+    pdate date DEFAULT '2022-01-08'
+)
+    RETURNS TABLE
+            (
+                pizzeria_name varchar
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT DISTINCT p.name AS pizzeria_name
+        FROM person_visits pv
+                 JOIN person_order po
+                      ON po.person_id = pv.person_id
+                          AND po.menu_id IN
+                              (SELECT id
+                               FROM menu
+                               WHERE price < pprice
+                                 AND pizzeria_id = pv.pizzeria_id)
+                 JOIN pizzeria p ON p.id = pv.pizzeria_id
+                 JOIN person per ON per.id = pv.person_id
+        WHERE per.name = pperson
+          AND pv.visit_date = pdate;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Вариант №3.
+CREATE OR REPLACE FUNCTION fnc_person_visits_and_eats_on_date_3(
+    pperson varchar DEFAULT 'Dmitriy',
+    pprice numeric DEFAULT 500,
+    pdate date DEFAULT '2022-01-08'
+)
+    RETURNS TABLE
+            (
+                pizzeria_name varchar
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT DISTINCT p.name AS pizzeria_name
+        FROM person_visits pv
+                 JOIN person_order po ON po.person_id = pv.person_id
+                 JOIN menu m ON m.id = po.menu_id AND m.price <= pprice AND m.pizzeria_id = pv.pizzeria_id
+                 JOIN pizzeria p ON p.id = pv.pizzeria_id
+                 JOIN person per ON per.id = pv.person_id AND per.name = pperson
+        WHERE pv.visit_date = pdate;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Вариант №1.
 SELECT *
 FROM fnc_person_visits_and_eats_on_date(pprice := 800);
 
 SELECT *
 FROM fnc_person_visits_and_eats_on_date(pperson := 'Anna', pprice := 1300, pdate := '2022-01-01');
+
+-- Вариант №2.
+SELECT *
+FROM fnc_person_visits_and_eats_on_date_2(pprice := 800);
+
+SELECT *
+FROM fnc_person_visits_and_eats_on_date_2(pperson := 'Anna', pprice := 1300, pdate := '2022-01-01');
+
+-- Вариант №3.
+SELECT *
+FROM fnc_person_visits_and_eats_on_date_3(pprice := 800);
+
+SELECT *
+FROM fnc_person_visits_and_eats_on_date_3(pperson := 'Anna', pprice := 1300, pdate := '2022-01-01');
