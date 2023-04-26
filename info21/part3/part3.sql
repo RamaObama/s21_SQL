73639ade13fcad01362a5c63ce1e680760292aec
rama <i@rrmstu.ru>
2023-04-26 20:30:58 +0500

    :sparkles: :: Function list task successfully

--- info21/part3/part3.sql
+++ info21/part3/part3.sql
@@ -25,5 +25,32 @@ END;
 $$
     LANGUAGE plpgsql;
 
-SELECT *
-FROM human_readable_points();
+-- SELECT * FROM human_readable_points();
+
+-- 2) Write a function that returns a table of the following form: user name, name of the checked task, number of XP received
+-- Include in the table only tasks that have successfully passed the check (according to the Checks table).
+-- One task can be completed successfully several times. In this case, include all successful checks in the table.
+
+DROP FUNCTION IF EXISTS get_amount_xp_gained();
+
+CREATE OR REPLACE FUNCTION get_amount_xp_gained()
+    RETURNS TABLE
+            (
+                Peer VARCHAR,
+                Task TEXT,
+                XP   BIGINT
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY (SELECT c.peer,
+                         c.task,
+                         x.xpamount
+                  FROM xp x
+                           JOIN checks c ON x."Check" = c.id);
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- SELECT * FROM get_amount_xp_gained();
+
