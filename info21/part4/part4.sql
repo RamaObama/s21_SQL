f40ab1e39dbff55916e0a83a3f0176d5b023667a
rama <i@rrmstu.ru>
2023-05-07 23:56:01 +0500

    :sparkles: :: Create a stored procedure with an output parameter that outputs a list of names and parameters of all scalar user's SQL functions in the current database. Do not output function names without parameters. The names and the list of parameters must be in one string. The output parameter returns the number of functions found.

--- info21/part4/part4.sql
+++ info21/part4/part4.sql
@@ -30,3 +30,56 @@ $$
     LANGUAGE plpgsql;
 
 -- CALL prc_drop_table('tmp_table');
+
+-- 2) Create a stored procedure with an output parameter that outputs a list of names and parameters of all scalar user's SQL functions in the current database.
+-- Do not output function names without parameters. The names and the list of parameters must be in one string.
+-- The output parameter returns the number of functions found.
+
+DROP PROCEDURE IF EXISTS prc_get_scalar_functions CASCADE;
+
+CREATE OR REPLACE PROCEDURE prc_get_scalar_functions(
+    OUT num_functions INT,
+    OUT function_list TEXT
+)
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    func_name   TEXT;
+    func_params TEXT;
+    func        RECORD;
+BEGIN
+    num_functions := 0;
+    function_list := '';
+
+    FOR func IN
+        SELECT p.proname AS function_name, pg_get_function_arguments(p.oid) AS function_params
+        FROM pg_proc p
+                 JOIN pg_namespace n ON p.pronamespace = n.oid
+        WHERE n.nspname = 'public'
+          AND p.proargtypes = ''
+        LOOP
+            func_name := func.function_name;
+            func_params := func.function_params;
+
+            -- Only include functions with parameters
+            IF func_params != '' THEN
+                num_functions := num_functions + 1;
+                function_list := function_list || func_name || '(' || func_params || '), ';
+            END IF;
+        END LOOP;
+
+    -- Remove trailing comma and space from function list
+    function_list := SUBSTRING(function_list, 1, LENGTH(function_list) - 2);
+END;
+$$;
+
+-- DO
+-- $$
+--     DECLARE
+--         num_functions INT;
+--         function_list TEXT;
+--     BEGIN
+--         CALL prc_get_scalar_functions(num_functions, function_list);
+--         RAISE NOTICE 'Found % scalar functions: %', num_functions, function_list;
+--     END
+-- $$;
